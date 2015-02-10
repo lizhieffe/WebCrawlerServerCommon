2202322318e393e9836d1ce3ecdf3d2147e236a4
Zhi Li <lizhieffe@gmail.com>
2015-02-10 09:53:21 -0500

    add

--- src/main/java/utils/ConfigUtil.java
+++ src/main/java/utils/ConfigUtil.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigUtil {
 
 	public static String getMasterIp() {
@@ -10,4 +13,15 @@ public class ConfigUtil {
 	public static int getMasterPort() {
 		return 1;
 	}
+	
+//	public static void get() {
+//		try {
+//			Resource resource = new ClassPathResource("/my.properties");
+//			Properties props = PropertiesLoaderUtils.loadProperties(resource);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
 }
